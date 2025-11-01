7ffa19d069ab3c5656bec912c2840220d484ac53
soumayg9673 <soumayg9673@zohomail.in>
2025-11-02 04:28:21 +0530

    move login and register route under auth route

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import Zoho from "$lib/components/oauth2/Zoho.svelte";
   import Google from "$lib/components/oauth2/Google.svelte";
-  import main_image from "$lib/assets/images/register_screen.webp";
+  import main_image from "$lib/assets/images/auth_route.webp";
 </script>
 
 <svelte:head>
-  <title>Register | GoAnubhavBharat</title>
+  <title>Login | GoAnubhavBharat</title>
 </svelte:head>
 
-<section id="register-user" class="flex-none md:flex items-center h-screen">
+<section id="login" class="flex-none md:flex items-center h-screen">
   <div
     class="h-1/2 lg:h-full lg:w-3/4 w-full bg-cover bg-center"
     style={`background-image: url(${main_image});`}
@@ -16,10 +16,10 @@
   <div
     class="h-1/2 lg:w-1/4 flex flex-col bg-white p-18 md:p-24 rounded-md items-center"
   >
-    <h1 class="text-3xl font-medium">Sign up</h1>
+    <h1 class="text-3xl font-medium">Sign in</h1>
     <h2>to access portal via</h2>
 
-    <section id="register-oidc" class="flex flex-col mt-6">
+    <section id="login-oidc" class="flex flex-col mt-6">
       <form action="?/zoho" method="POST">
         <button type="submit">
           <Zoho />
@@ -32,9 +32,10 @@
       </form>
     </section>
 
-    <section id="register-note" class="mt-6 text-justify">
+    <section id="login-note" class="mt-6 text-justify">
       <p>
-        Join the movement toward Vision 2047 — sign up today and be a catalyst in driving Sustainable Tourism across India.
+        Continue your journey toward Vision 2047 — stay on track to promote
+        Sustainable Tourism and shape a responsible travel future for India.
       </p>
     </section>
   </div>
--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import Zoho from "$lib/components/oauth2/Zoho.svelte";
   import Google from "$lib/components/oauth2/Google.svelte";
-  import main_image from "$lib/assets/images/login_screen.webp";
+  import main_image from "$lib/assets/images/auth_route.webp";
 </script>
 
 <svelte:head>
-  <title>Login | GoAnubhavBharat</title>
+  <title>Register | GoAnubhavBharat</title>
 </svelte:head>
 
-<section id="login" class="flex-none md:flex items-center h-screen">
+<section id="register-user" class="flex-none md:flex items-center h-screen">
   <div
     class="h-1/2 lg:h-full lg:w-3/4 w-full bg-cover bg-center"
     style={`background-image: url(${main_image});`}
@@ -16,10 +16,10 @@
   <div
     class="h-1/2 lg:w-1/4 flex flex-col bg-white p-18 md:p-24 rounded-md items-center"
   >
-    <h1 class="text-3xl font-medium">Sign in</h1>
+    <h1 class="text-3xl font-medium">Sign up</h1>
     <h2>to access portal via</h2>
 
-    <section id="login-oidc" class="flex flex-col mt-6">
+    <section id="register-oidc" class="flex flex-col mt-6">
       <form action="?/zoho" method="POST">
         <button type="submit">
           <Zoho />
@@ -30,6 +30,13 @@
           <Google />
         </button>
       </form>
+    </section>
+
+    <section id="register-note" class="mt-6 text-justify">
+      <p>
+        Join the movement toward Vision 2047 — sign up today and be a catalyst
+        in driving Sustainable Tourism across India.
+      </p>
     </section>
   </div>
 </section>
