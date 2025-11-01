e2bff4c8d90a95d3bab834ce8f7947593d190737
soumayg9673 <soumayg9673@zohomail.in>
2025-11-02 03:31:48 +0530

    add login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import Zoho from "$lib/components/oauth2/Zoho.svelte";
+  import Google from "$lib/components/oauth2/Google.svelte";
+  import main_image from "$lib/assets/images/login_screen.webp";
+</script>
+
+<svelte:head>
+  <title>Login | GoAnubhavBharat</title>
+</svelte:head>
+
+<section id="login" class="flex-none md:flex items-center h-screen">
+  <div
+    class="h-1/2 lg:h-full lg:w-3/4 w-full bg-cover bg-center"
+    style={`background-image: url(${main_image});`}
+  ></div>
+  <div
+    class="h-1/2 lg:w-1/4 flex flex-col bg-white p-18 md:p-24 rounded-md items-center"
+  >
+    <h1 class="text-3xl font-medium">Sign in</h1>
+    <h2>to access portal via</h2>
+
+    <section id="login-oidc" class="flex flex-col mt-6">
+      <form action="?/zoho" method="POST">
+        <button type="submit">
+          <Zoho />
+        </button>
+      </form>
+      <form action="?/google" method="POST">
+        <button type="submit">
+          <Google />
+        </button>
+      </form>
+    </section>
+  </div>
+</section>
