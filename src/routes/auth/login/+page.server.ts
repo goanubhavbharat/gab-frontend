5a57ac996c4b059be043de0075ba062c9fc6cc65
soumayg9673 <soumayg9673@zohomail.in>
2025-12-15 23:00:33 +0530

    update oauth login url

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -4,9 +4,9 @@ import { PRIVATE_API_URL } from '$env/static/private';
 
 export const actions: Actions = {
     zoho: async() => {
-        throw redirect(302, `${PRIVATE_API_URL}/auth/v1/oauth/zoho/login`);
+        throw redirect(302, `${PRIVATE_API_URL}/auth/v1/oauth/zoho/init`);
     },
     google: async() => {
-        throw redirect(302, `${PRIVATE_API_URL}/auth/v1/oauth/google/login`);
+        throw redirect(302, `${PRIVATE_API_URL}/auth/v1/oauth/google/init`);
     }
 }
