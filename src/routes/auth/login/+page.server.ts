2e49741808eabccd9cf965f96725c004824d1508
soumayg9673 <soumayg9673@zohomail.in>
2025-12-18 16:53:12 +0530

    prevent loggedin user to open login or register route

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import type { Actions } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import { PRIVATE_API_URL } from '$env/static/private';
 
 export const actions: Actions = {
@@ -8,5 +8,12 @@ export const actions: Actions = {
     },
     google: async() => {
         throw redirect(302, `${PRIVATE_API_URL}/auth/v1/oauth/google/init`);
+    }
+}
+
+export const load: PageServerLoad = async({ cookies }) => {
+    const token = cookies.get("sessionId")
+    if (token) {
+        throw redirect(302, '/');
     }
 }
